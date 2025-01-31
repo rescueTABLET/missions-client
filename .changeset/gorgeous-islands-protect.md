52f4c37ae992ede8605235fbd899c45d75c6f43e
Philipp Jardas <philipp@jardas.de>
2025-01-31 14:23:43 +0100

    Update @hey-api/openapi-ts

---
+++ .changeset/gorgeous-islands-protect.md
@@ -0,0 +1,5 @@
+---
+"@rescuetablet/missions-client": patch
+---
+
+Update @hey-api/openapi-ts
--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "@hey-api/openapi-ts";
+import { defaultPlugins, defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
   input: "https://missions-api.rescuetablet.com/openapi.yaml",
-  client: "@hey-api/client-fetch",
   output: {
     path: "src/client",
     clean: true,
     format: "prettier",
   },
+  plugins: [...defaultPlugins, "@hey-api/client-fetch"],
 });
